9f66e56a7d1c515e57ffae39bd236089b87ca660
Yuuki Takagi <64290748+takagiyuuki@users.noreply.github.com>
2023-07-20 07:04:24 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My First Gatsby Site`,
+    title: `Portfolio website`,
     description: `Example project for the Gatsby Head API`,
     twitterUsername: `@gatsbyjs`,
     siteUrl: `https://www.yourdomain.tld`,
