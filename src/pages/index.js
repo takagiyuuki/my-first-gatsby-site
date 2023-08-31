745f9b1509fe126cf6ef3278570e5d7838eeca82
Yuuki Takagi <snow7777man@gmail.com>
2023-08-31 09:00:21 +0000

    Create contact page, Update CSS module

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
 import { Seo } from "../components/seo";
+import { imageCenter } from "../components/styles/homepage.module.scss";
 
 const IndexPage = () => {
   return (
@@ -10,6 +11,7 @@ const IndexPage = () => {
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        className={imageCenter}
       />
     </Layout>
   );
