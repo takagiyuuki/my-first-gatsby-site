1357174b15769c0a78b094c55742a4ab63bc2e67
Yuuki Takagi <snow7777man@gmail.com>
2023-09-01 07:38:12 +0000

    update navigation bar design

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import {
+  headContainer,
   container,
   heading,
   navBar,
@@ -24,40 +25,42 @@ const Layout = ({ pageTitle, children }) => {
   `);
   return (
     <div className={container}>
-      <header>
-        <Link to="/" className={siteTitle}>
-          {data.site.siteMetadata.title}
-        </Link>
-      </header>
-      <nav className={navBar}>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/contact" className={navLinkText}>
-              Contact
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="https://notion-blog-8kd.pages.dev" className={navLinkText}>
-              Blog
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/services" className={navLinkText}>
-              Services
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/artwork" className={navLinkText}>
-              Artwork
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={headContainer}>
+        <header>
+          <Link to="/" className={siteTitle}>
+            {data.site.siteMetadata.title}
+          </Link>
+        </header>
+        <nav className={navBar}>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/about" className={navLinkText}>
+                About
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/contact" className={navLinkText}>
+                Contact
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="https://notion-blog-8kd.pages.dev" className={navLinkText}>
+                Blog
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/services" className={navLinkText}>
+                Services
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/artwork" className={navLinkText}>
+                Artwork
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <main>
         <h1 className={heading}>{pageTitle}</h1>
         {children}
