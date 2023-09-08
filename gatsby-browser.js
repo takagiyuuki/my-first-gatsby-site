70f43d1d28c0433988f1eae341d3a2b52c738472
Yuuki Takagi <snow7777man@gmail.com>
2023-09-08 01:30:54 +0000

    fixed sass valiables setting fixed src/pafes/index.js add npm package "babel-plugin-lodash"

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import "./src/styles/global-styles.scss";
+// import "./src/styles/global-styles.scss";
 import { rootWrapper } from "./gatsby-wrapper";
 
 export const wrapRootElement = rootWrapper;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,46 @@
-import React from "react";
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
+import { Seo } from "../components/seo";
+import styles from "../styles/content.module.scss";
 import { useTranslation } from "react-i18next";
 
-const Index = () => {
+const IndexPage = () => {
   const { t, i18n } = useTranslation();
-
   const changeLang = (lang) => {
     i18n.changeLanguage(lang);
   };
 
   return (
-    <div>
-      <h1>{t("topPage.greeting")}</h1>
-      <ul>
-        <li>
-          <button onClick={() => changeLang("en")}>EN</button>
-        </li>
-        <li>
-          <button onClick={() => changeLang("de")}>DE</button>
-        </li>
-        <li>
-          <button onClick={() => changeLang("ja")}>JA</button>
-        </li>
-      </ul>
-    </div>
+    <Layout pageTitle={t("topPage.greeting")}>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <div className={styles.bodyFlex}>
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+          className={styles.bodyImage}
+        />
+        <div className={styles.bodyText}>
+          <h2>Hi! I'm Yuki</h2>
+          <p>Welcome My branding site.</p>
+          <p>This website is my branding pages.</p>
+          <ul>
+            <li>
+              <button onClick={() => changeLang("en")}>EN</button>
+            </li>
+            <li>
+              <button onClick={() => changeLang("de")}>DE</button>
+            </li>
+            <li>
+              <button onClick={() => changeLang("ja")}>JA</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
-export default Index;
+export const Head = () => <Seo title="Home Page" />;
+
+export default IndexPage;
