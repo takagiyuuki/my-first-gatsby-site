27e1433b735544156c3af783df9d24d8210090cb
Yuuki Takagi <snow7777man@gmail.com>
2023-10-16 18:01:09 +0900

    feat: create post.js

---
+++ src/templates/post.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const post = () => {
+  return (
+    <Layout>
+      <div>ブログ記事のコンテンツ</div>
+    </Layout>
+  );
+};
+
+export default post;
