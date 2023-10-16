2ba93e5c7d4000b820a946f087de9907f63d2cfd
Yuuki Takagi <snow7777man@gmail.com>
2023-10-16 18:01:29 +0900

    update 1 file and delete 1 file

--- src/pages/artwork/index.js
+++ src/pages/artwork/index.js
@@ -1,45 +1,14 @@
 import * as React from "react";
-import { Link, graphql } from "gatsby";
 import { Layout } from "../../components/layout";
 import { Seo } from "../../components/seo";
-import * as styles from "../../styles/content.module.scss";
 
-const ArtworkPage = ({ data }) => {
+const Artwork = () => {
   return (
-    <Layout pageTitle="My Artwork">
-      {data.allMdx.nodes.map((node) => (
-        <article key={node.id}>
-          <h2>
-            <Link
-              to={`/artwork/${node.frontmatter.slug}`}
-              className={styles.linkTitle}
-            >
-              {node.frontmatter.title}
-            </Link>
-          </h2>
-          <p>Posted: {node.frontmatter.date}</p>
-          <p>{node.excerpt}</p>
-        </article>
-      ))}
+    <Layout pageTitle="Contact">
+      <h1>this is test</h1>
     </Layout>
   );
 };
 
-export const query = graphql`
-  query {
-    allMdx(sort: { frontmatter: { date: DESC } }) {
-      nodes {
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          title
-          slug
-        }
-        id
-        excerpt
-      }
-    }
-  }
-`;
-
-export const Head = () => <Seo title="My Artwork" />;
-export default ArtworkPage;
+export const Head = () => <Seo title="Artwork" />;
+export default Artwork;
