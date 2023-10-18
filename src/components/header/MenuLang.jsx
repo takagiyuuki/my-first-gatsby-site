f4e523f3725c6470971b9b171e3c94bf23346fa2
Yuuki Takagi <snow7777man@gmail.com>
2023-10-18 15:50:42 +0900

    feat: create MenuLang.jsx, src/components/header/index.jsx and menuHamburger.jsx

---
+++ src/components/header/MenuLang.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import * as styles from "../styles/menuLang.module.scss";
+
+export const MenuLang = () => {
+  return (
+    <div>
+      <input type="checkbox" className={styles.menuBtn} id="menu-btn" />
+      <label htmlfor={menuBtn} className={styles.menuIcon}>
+        <span className={styles.navicon}></span>
+      </label>
+      <ul className={styles.menu}>
+        <li className={styles.top}>
+          <p>English</p>
+        </li>
+        <li>
+          <p>Español</p>
+        </li>
+        <li>
+          <p>日本語</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
