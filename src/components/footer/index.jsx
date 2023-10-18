739245d6d048a11949144dc7a582cd279911c07a
Yuuki Takagi <snow7777man@gmail.com>
2023-10-18 15:50:34 +0900

    feat: create src/components/footer/index.jsx

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import * as styles from "../../styles/footer.module.scss";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.footerTitle}>© 2023 Takagi Yuki</p>
+      <a
+        href="https://github.com/takagiyuuki"
+        target="_blank"
+        rel="noreferrer"
+        className={styles.footerLogoGithub}
+      >
+        <StaticImage
+          alt="Github Link"
+          src="../../images/github-mark.svg"
+          className={styles.footerLogoGithubBlack}
+        />
+        <StaticImage
+          alt="Github Link"
+          src="../../images/github-mark-white.svg"
+          className={styles.footerLogoGithubWhite}
+        />
+      </a>
+    </footer>
+  );
+};
