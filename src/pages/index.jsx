8c374c4821e75cc76ffd773905d0092f9019cba2
Yuuki <64290748+takagiyuuki@users.noreply.github.com>
2023-10-25 17:25:11 +0000

    update src/pages/index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import { Layout } from "../layouts";
 import { StaticImage } from "gatsby-plugin-image";
 
 import { Seo } from "../components/seo";
-import * as styles from "../styles/content.module.scss";
+// import * as styles from "../styles/content.module.scss";
 import { useTranslation } from "react-i18next";
 
 import { Contact } from "../components/contact";
@@ -14,20 +14,14 @@ const IndexPage = () => {
 
   return (
     <Layout pageTitle={t("topPage.greeting")}>
-      {/* <MenuLang /> */}
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <div className={styles.bodyFlex}>
-        <StaticImage
-          alt="Static"
-          src="https://source.unsplash.com/featured/?rain"
-          className={styles.bodyImage}
-        />
-        <div className={styles.bodyText}>
-          <p>{t("topPage.introText")}</p>
-          <About />
-          <Contact />
-        </div>
-      </div>
+      <StaticImage
+        alt="Static"
+        src="https://source.unsplash.com/featured/?rain"
+      />
+      <p>{t("topPage.introText")}</p>
+      <About />
+      <Contact />
     </Layout>
   );
 };
