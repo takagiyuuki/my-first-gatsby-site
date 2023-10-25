9a2b06447a0ee803ab418d489e128dd5e5b06fb6
Yuuki Takagi <snow7777man@gmail.com>
2023-10-25 16:15:41 +0900

    update src/components/footer/index.jsx and sample.jsx update sample.jsx

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,45 +1,28 @@
-// import * as React from "react";
-// import { StaticImage } from "gatsby-plugin-image";
-// import * as styles from "../../styles/footer.module.scss";
-
-// export const Footer = () => {
-//   return (
-//     <footer className={styles.footer}>
-//       <p className={styles.footerTitle}>© 2023 Takagi Yuki</p>
-//       <a
-//         href="https://github.com/takagiyuuki"
-//         target="_blank"
-//         rel="noreferrer"
-//         className={styles.footerLogoGithub}
-//       >
-//         <StaticImage
-//           alt="Github Link"
-//           src="../../images/github-mark.svg"
-//           className={styles.footerLogoGithubBlack}
-//         />
-//         <StaticImage
-//           alt="Github Link"
-//           src="../../images/github-mark-white.svg"
-//           className={styles.footerLogoGithubWhite}
-//         />
-//       </a>
-//     </footer>
-//   );
-// };
-
 import * as React from "react";
+// MUI
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+// Icons
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EggAltIcon from "@mui/icons-material/EggAlt";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { SiLinktree } from "react-icons/si";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
       {"Copyright © "}
-      <Link color="inherit" href="https://github.com/takagiyuuki/portfolio">
+      <Link
+        color="inherit"
+        href="https://github.com/takagiyuuki/portfolio"
+        underline="hover"
+      >
         Yuki Takagi
       </Link>{" "}
       {new Date().getFullYear()}
@@ -52,6 +35,35 @@ const defaultTheme = createTheme();
 
 // export default function Footer() {
 export function Footer() {
+  const SiteIcon = ({ children }) => {
+    return (
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 1 }}
+        href="/"
+      >
+        {children}
+      </IconButton>
+    );
+  };
+  const FooterIcon = ({ link, children }) => {
+    return (
+      <IconButton
+        size="small"
+        edge="end"
+        color="inherit"
+        aria-label="menu"
+        // sx={{ mr: 1 }}
+        href={link}
+        target="_blank"
+      >
+        {children}
+      </IconButton>
+    );
+  };
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
@@ -67,11 +79,27 @@ export function Footer() {
               : theme.palette.grey[800],
         }}
       >
-        <Container maxWidth="sm">
-          <Typography variant="body1">
-            My sticky footer can be found here.
-          </Typography>
-          <Copyright />
+        <Container maxWidth="md">
+          <Toolbar>
+            <SiteIcon>
+              <EggAltIcon />
+            </SiteIcon>
+            <Box sx={{ px: 2, mx: 2 }}>
+              <Typography variant="body1">Contact me:)</Typography>
+              <Copyright />
+            </Box>
+            <Box sx={{ px: 2, mx: 2 }}>
+              <FooterIcon link={"https://github.com/takagiyuuki"}>
+                <GitHubIcon />
+              </FooterIcon>
+              <FooterIcon link={"https://www.linkedin.com/in/takagiyuuki"}>
+                <LinkedInIcon />
+              </FooterIcon>
+              <FooterIcon link={"https://linktr.ee/yuukitakagi"}>
+                <SiLinktree />
+              </FooterIcon>
+            </Box>
+          </Toolbar>
         </Container>
       </Box>
     </ThemeProvider>
--- src/components/header/sample.jsx
+++ src/components/header/sample.jsx
@@ -4,9 +4,7 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-// import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-// import MenuIcon from "@mui/icons-material/Menu";
 import CssBaseline from "@mui/material/CssBaseline";
 import Link from "@mui/material/Link";
 // Icons
@@ -49,17 +47,6 @@ export const HeaderBar = () => {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" size="medium">
           <Toolbar>
-            {/* <IconButton
-              size="medium"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 1 }}
-              href="/"
-            >
-              <MdEggAlt />
-              <LuEggFried />
-            </IconButton> */}
             <HeaderIcon>
               <MdEggAlt />
               <LuEggFried />
@@ -78,61 +65,11 @@ export const HeaderBar = () => {
               <HeaderLink link={"https://notion-blog-8kd.pages.dev"}>
                 Blog
               </HeaderLink>
-              {/* <Link
-                color="inherit"
-                underline="hover"
-                href="/about"
-                sx={{ px: 2, mt: "auto" }}
-              >
-                About
-              </Link>
-              <Link
-                color="inherit"
-                underline="hover"
-                href="/contact"
-                sx={{ px: 2, mt: "auto" }}
-              >
-                Contact
-              </Link>
-              <Link
-                color="inherit"
-                underline="hover"
-                href="/services"
-                sx={{ px: 2, mt: "auto" }}
-              >
-                Services
-              </Link>
-              <Link
-                color="inherit"
-                underline="hover"
-                href="/artwork"
-                sx={{ px: 2, mt: "auto" }}
-              >
-                Artwork
-              </Link>
-              <Link
-                color="inherit"
-                underline="hover"
-                href="https://notion-blog-8kd.pages.dev"
-                target="_blank"
-                rel="noopener"
-                sx={{ px: 2, mt: "auto" }}
-              >
-                Blog
-              </Link> */}
             </Typography>
-            {/* <IconButton
-              size="large"
-              edge="end"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            > */}
             <HeaderIcon>
               <HeaderMenuLang />
               <HeaderMenuIcon />
             </HeaderIcon>
-            {/* </IconButton> */}
           </Toolbar>
         </AppBar>
       </Box>
