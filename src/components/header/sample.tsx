cf20353f6a29aa638f3f8852aa2adf63d3ac24e2
Yuuki Takagi <snow7777man@gmail.com>
2023-10-30 18:02:33 +0900

    update sample.tsx

--- src/components/header/sample.tsx
+++ src/components/header/sample.tsx
@@ -6,45 +6,45 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
-import useScrollTrigger from "@mui/material/useScrollTrigger";
-import Slide from "@mui/material/Slide";
+// import useScrollTrigger from "@mui/material/useScrollTrigger";
+// import Slide from "@mui/material/Slide";
 // Icons
 import { MdEggAlt } from "react-icons/md";
-import { LuEggFried } from "react-icons/lu";
+// import { LuEggFried } from "react-icons/lu";
 // LocalComponents
 import { HeaderMenuIcon } from "./HeaderMenuIcon";
 import { HeaderMenuLang } from "./HeaderMenuLang";
 
-import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import Fade from '@mui/material/Fade';
+// import Box from '@mui/material/Box';
+// import Fab from '@mui/material/Fab';
+// import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+// import Fade from '@mui/material/Fade';
 
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  // window?: () => Window;
-  children: React.ReactElement;
-}
+// interface Props {
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * You won't need it on your project.
+//    */
+//   // window?: () => Window;
+//   children: React.ReactElement;
+// }
 
-function HideOnScroll(props: Props) {
-  const { children } = props;
-  // const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    // target: window ? window() : undefined,
-  });
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
+// function HideOnScroll(props: Props) {
+//   const { children } = props;
+//   // const { children, window } = props;
+//   // Note that you normally won't need to set the window ref as useScrollTrigger
+//   // will default to window.
+//   // This is only being set here because the demo is in an iframe.
+//   const trigger = useScrollTrigger({
+//     // target: window ? window() : undefined,
+//   });
+//   return (
+//     <Slide appear={false} direction="down" in={!trigger}>
+//       {children}
+//     </Slide>
+//   );
+// }
 
 // function ElevationScroll(props: Props) {
 //   const { children } = props;
@@ -62,41 +62,42 @@ function HideOnScroll(props: Props) {
 //   });
 // }
 
-function ScrollTop(props: Props) {
-  const { children } = props;
-  // const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    // target: window ? window() : undefined,
-    disableHysteresis: true,
-    threshold: 100,
-  });
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    const anchor = (
-      (event.target as HTMLDivElement).ownerDocument || document
-    ).querySelector('#back-to-top-anchor');
-    if (anchor) {
-      anchor.scrollIntoView({
-        block: 'center',
-      });
-    }
-  };
-  return (
-    <Fade in={trigger}>
-      <Box
-        onClick={handleClick}
-        role="presentation"
-        sx={{ position: 'fixed', bottom: 16, right: 16 }}
-      >
-        {children}
-      </Box>
-    </Fade>
-  );
-}
+// function ScrollTop(props: Props) {
+//   const { children } = props;
+//   // const { children, window } = props;
+//   // Note that you normally won't need to set the window ref as useScrollTrigger
+//   // will default to window.
+//   // This is only being set here because the demo is in an iframe.
+//   const trigger = useScrollTrigger({
+//     // target: window ? window() : undefined,
+//     disableHysteresis: true,
+//     threshold: 100,
+//   });
+//   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+//     const anchor = (
+//       (event.target as HTMLDivElement).ownerDocument || document
+//     ).querySelector('#back-to-top-anchor');
+//     if (anchor) {
+//       anchor.scrollIntoView({
+//         block: 'center',
+//       });
+//     }
+//   };
+//   return (
+//     <Fade in={trigger}>
+//       <Box
+//         onClick={handleClick}
+//         role="presentation"
+//         sx={{ position: 'fixed', bottom: 16, right: 16 }}
+//       >
+//         {children}
+//       </Box>
+//     </Fade>
+//   );
+// }
 
-export const HeaderBar = (props: Props) => {
+export const HeaderBar = () => {
+// export const HeaderBar = (props: Props) => {
   const SiteIcon = ({ children }) => {
     return (
       <IconButton
@@ -138,14 +139,14 @@ export const HeaderBar = (props: Props) => {
   };
   return (
     <>
-      <HideOnScroll {...props}>
-        <ElevationScroll {...props}>
+      {/* <HideOnScroll {...props}> */}
+        {/* <ElevationScroll {...props}> */}
           {/* <Box sx={{ flexGrow: 1 }}> */}
           <AppBar enableColorOnDark={true}>
             <Toolbar sx={{ display: "flex" }}>
               <SiteIcon>
                 <MdEggAlt />
-                <LuEggFried />
+                {/* <LuEggFried /> */}
               </SiteIcon>
               <Typography
                 variant="h6"
@@ -169,9 +170,9 @@ export const HeaderBar = (props: Props) => {
             </Toolbar>
           </AppBar>
           {/* </Box> */}
-        </ElevationScroll>
-      <Toolbar />
-      </HideOnScroll>
+        {/* </ElevationScroll>
+      <Toolbar /> */}
+      {/* </HideOnScroll> */}
     </>
   );
 };
