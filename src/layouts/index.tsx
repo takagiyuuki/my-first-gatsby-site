a8742ad7797f9f790df2284ab9b678016342d0a1
Yuuki Takagi <snow7777man@gmail.com>
2023-11-01 09:26:55 +0900

    delete 1 file and update 1 file

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -30,11 +30,20 @@ const theme = createTheme({
   },
 });
 
-export function Layout(pageTitle, children) {
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+export function Layout(pageTitle, children, props: Props) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <HideOnScroll>
+      <HideOnScroll {...props}>
         <HeaderBar />
       </HideOnScroll>
       <Toolbar id="back-to-top-anchor" />
@@ -44,14 +53,6 @@ export function Layout(pageTitle, children) {
             {pageTitle}
           </Typography>
           {children}
-          {[...new Array(12)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-            )
-            .join("\n")}
         </Box>
       </Container>
       <ScrollTop>
