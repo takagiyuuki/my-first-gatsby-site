d32cffee3c6e847b38b597bffeda733721cc3f22
Yuuki Takagi <snow7777man@gmail.com>
2023-11-06 19:41:05 +0900

    update sample.tsx and src/pages/index.jsx

--- src/components/header/sample.tsx
+++ src/components/header/sample.tsx
@@ -1,25 +1,19 @@
-import * as React from "react";
+import * as React from 'react';
 // MUI
-import AppBar from "@mui/material/AppBar";
-// import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import Link from "@mui/material/Link";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
 // import useScrollTrigger from "@mui/material/useScrollTrigger";
 // import Slide from "@mui/material/Slide";
 // Icons
-import { MdEggAlt } from "react-icons/md";
-// import { LuEggFried } from "react-icons/lu";
+import { MdEggAlt } from 'react-icons/md';
 // LocalComponents
-import { HeaderMenuIcon } from "./HeaderMenuIcon";
-import { HeaderMenuLang } from "./HeaderMenuLang";
-
-// import Box from '@mui/material/Box';
-// import Fab from '@mui/material/Fab';
-// import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-// import Fade from '@mui/material/Fade';
-
+import { HeaderMenuIcon } from './HeaderMenuIcon';
+import { HeaderMenuLang } from './HeaderMenuLang';
+import { HideOnScroll } from '../AppBar/HideAppBar';
 
 // interface Props {
 //   /**
@@ -30,8 +24,9 @@ import { HeaderMenuLang } from "./HeaderMenuLang";
 //   children: React.ReactElement;
 // }
 
-// function HideOnScroll(props: Props) {
-//   const { children } = props;
+// // function HideOnScroll(props: Props) {
+// const HideOnScroll = ({ children: Props }) => {
+//   // const { children } = props;
 //   // const { children, window } = props;
 //   // Note that you normally won't need to set the window ref as useScrollTrigger
 //   // will default to window.
@@ -44,135 +39,83 @@ import { HeaderMenuLang } from "./HeaderMenuLang";
 //       {children}
 //     </Slide>
 //   );
-// }
+// };
 
-// function ElevationScroll(props: Props) {
-//   const { children } = props;
-//   // const { children, window } = props;
-//   // Note that you normally won't need to set the window ref as useScrollTrigger
-//   // will default to window.
-//   // This is only being set here because the demo is in an iframe.
-//   const trigger = useScrollTrigger({
-//     disableHysteresis: true,
-//     threshold: 0,
-//     // target: window ? window() : undefined,
-//   });
-//   return React.cloneElement(children, {
-//     elevation: trigger ? 4 : 0,
-//   });
-// }
-
-// function ScrollTop(props: Props) {
-//   const { children } = props;
-//   // const { children, window } = props;
-//   // Note that you normally won't need to set the window ref as useScrollTrigger
-//   // will default to window.
-//   // This is only being set here because the demo is in an iframe.
-//   const trigger = useScrollTrigger({
-//     // target: window ? window() : undefined,
-//     disableHysteresis: true,
-//     threshold: 100,
-//   });
-//   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-//     const anchor = (
-//       (event.target as HTMLDivElement).ownerDocument || document
-//     ).querySelector('#back-to-top-anchor');
-//     if (anchor) {
-//       anchor.scrollIntoView({
-//         block: 'center',
-//       });
-//     }
-//   };
-//   return (
-//     <Fade in={trigger}>
-//       <Box
-//         onClick={handleClick}
-//         role="presentation"
-//         sx={{ position: 'fixed', bottom: 16, right: 16 }}
-//       >
-//         {children}
-//       </Box>
-//     </Fade>
-//   );
-// }
-
-export const HeaderBar = () => {
+// export const HeaderBar = () => {
 // export const HeaderBar = (props: Props) => {
-  const SiteIcon = ({ children }) => {
+export function HeaderBar() {
+    function SiteIcon({ children }) {
+        return (
+            <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ justifyContent: 'flex-start' }}
+            href="/"
+          >
+            {children}
+          </IconButton>
+        );
+    }
+    function HeaderIcon({ children }) {
+        return (
+            <IconButton
+            size="medium"
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            sx={{ justifyContent: 'flex-end' }}
+          >
+            {children}
+          </IconButton>
+        );
+    }
+    function HeaderLink({ link, children }) {
+        return (
+            <Link
+            color="inherit"
+            underline="hover"
+            href={link}
+            sx={{ px: 2, mt: 'auto' }}
+          >
+            {children}
+          </Link>
+        );
+    }
     return (
-      <IconButton
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="menu"
-        sx={{ justifyContent: "flex-start" }}
-        href="/"
-      >
-        {children}
-      </IconButton>
+        <>
+            {/* <HideOnScroll {...props}> */}
+            <HideOnScroll>
+                {/* <AppBar enableColorOnDark={true}> */}
+                <AppBar>
+                    <Toolbar>
+                        <SiteIcon>
+                            <MdEggAlt />
+                      </SiteIcon>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ justifyContent: 'center' }}
+                      >
+                            <HeaderLink link="/about">About</HeaderLink>
+                            <HeaderLink link="/contact">Contact</HeaderLink>
+                            <HeaderLink link="/services">Services</HeaderLink>
+                            <HeaderLink link="/artwork">Artwork</HeaderLink>
+                            <HeaderLink link="https://notion-blog-8kd.pages.dev">
+                            Blog
+                            </HeaderLink>
+                      </Typography>
+                        <HeaderIcon>
+                            <HeaderMenuLang />
+                      </HeaderIcon>
+                        <HeaderIcon>
+                            <HeaderMenuIcon />
+                      </HeaderIcon>
+                  </Toolbar>
+              </AppBar>
+                <Toolbar />
+          </HideOnScroll>
+      </>
     );
-  };
-  const HeaderIcon = ({ children }) => {
-    return (
-      <IconButton
-        size="medium"
-        edge="end"
-        color="inherit"
-        aria-label="menu"
-        sx={{ justifyContent: "flex-end" }}
-      >
-        {children}
-      </IconButton>
-    );
-  };
-  const HeaderLink = ({ link, children }) => {
-    return (
-      <Link
-        color="inherit"
-        underline="hover"
-        href={link}
-        sx={{ px: 2, mt: "auto" }}
-      >
-        {children}
-      </Link>
-    );
-  };
-  return (
-    <>
-      {/* <HideOnScroll {...props}> */}
-        {/* <ElevationScroll {...props}> */}
-          {/* <Box sx={{ flexGrow: 1 }}> */}
-          <AppBar enableColorOnDark={true}>
-            <Toolbar sx={{ display: "flex" }}>
-              <SiteIcon>
-                <MdEggAlt />
-                {/* <LuEggFried /> */}
-              </SiteIcon>
-              <Typography
-                variant="h6"
-                component="div"
-                sx={{ justifyContent: "center" }}
-              >
-                <HeaderLink link={"/about"}>About</HeaderLink>
-                <HeaderLink link={"/contact"}>Contact</HeaderLink>
-                <HeaderLink link={"/services"}>Services</HeaderLink>
-                <HeaderLink link={"/artwork"}>Artwork</HeaderLink>
-                <HeaderLink link={"https://notion-blog-8kd.pages.dev"}>
-                  Blog
-                </HeaderLink>
-              </Typography>
-              <HeaderIcon>
-                <HeaderMenuLang />
-              </HeaderIcon>
-              <HeaderIcon>
-                <HeaderMenuIcon />
-              </HeaderIcon>
-            </Toolbar>
-          </AppBar>
-          {/* </Box> */}
-        {/* </ElevationScroll>
-      <Toolbar /> */}
-      {/* </HideOnScroll> */}
-    </>
-  );
-};
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,31 @@
-import * as React from "react";
-import { Layout } from "../layouts";
-import { StaticImage } from "gatsby-plugin-image";
+import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Layout } from '../layouts';
 
-import { Seo } from "../components/seo";
+import { Seo } from '../components/seo';
 // import * as styles from "../styles/content.module.scss";
 
-import { Contact } from "../components/contact";
-import About from "../content/about.mdx";
+import { Contact } from '../components/contact';
+import About from '../content/about.mdx';
 
-const IndexPage = () => {
-  return (
-    <Layout pageTitle="Hi">
-      <StaticImage
-        alt="Static"
-        src="https://source.unsplash.com/featured/?rain"
-      />
-      <p>
-        Welcome My branding site.I'm Yuki.This website is my branding pages.
-      </p>
-      <About />
-      <Contact />
-    </Layout>
-  );
-};
+function IndexPage() {
+    return (
+        <Layout pageTitle="Hi">
+            <StaticImage
+                alt="Static"
+                src="https://source.unsplash.com/featured/?rain"
+          />
+            <p>
+            Welcome My branding site.I'm Yuki.This website is my branding pages.
+            </p>
+            <About />
+            <Contact />
+      </Layout>
+    );
+}
 
-export const Head = () => <Seo title="Home Page" />;
+export function Head() {
+    return <Seo title="Home Page" />;
+}
 
 export default IndexPage;
